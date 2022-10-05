e27bf7a75f3befa9c1d310c19a06df3f1a9cc7e2
Petra Vrobelova <petravrobelova98@gmail.com>
2022-10-05 15:55:27 +0200

    first commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
+export default class TodoItem extends Component {
+    render() {
+
+        const {title, handleDelete, handleEdit} = this.props
+
+        return (
+            <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
+                <h6>{title}</h6>
+                <div className="todo-icon">
+                    <span className="mx-2 text-success" onClick={handleEdit}>
+                        <i className="fas fa-pen"/>
+                    </span>
+                    <span className="mx-2 text-dangerous" onClick={handleDelete}>
+                        <i className="fas fa-trash"/>
+                    </span>
+                </div>
+            </li>
+        )
+    }
+}
