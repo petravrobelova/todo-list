9ec6f0ef537a37138433f0bac5472c993d25d664
Petra Vrobelova <petravrobelova98@gmail.com>
2022-10-05 16:00:33 +0200

    Updates

---
+++ precache-manifest.6f209f102e8cc5db531cb197444ea29e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8a05438dde3f86899f753bcda8c3b65",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "db48fc3ce572a3a273f9",
+    "url": "/todo-list/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "7a914b8c21e6733daed0",
+    "url": "/todo-list/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "db48fc3ce572a3a273f9",
+    "url": "/todo-list/static/js/2.cbd86459.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.cbd86459.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a914b8c21e6733daed0",
+    "url": "/todo-list/static/js/main.27870eeb.chunk.js"
+  },
+  {
+    "revision": "a557b42a7f266985029e",
+    "url": "/todo-list/static/js/runtime-main.9c8ee466.js"
+  }
+]);
